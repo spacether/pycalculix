cf096e4bc1b408984b67ed41326d9e82ae9980b7
spacether <janthonyb1@hotmail.com>
2015-03-17 18:22:09 -0400

    0.9.3
    Added code in environment.py which limits checking for high DPI windows
    monitors to Windows 8 or higher.
    This fixes Issue #3

--- pycalculix/environment.py
+++ pycalculix/environment.py
@@ -11,14 +11,45 @@ Attributes:
 import sys # needed to check if 32 or 64 bit interpreter
 import os # used to delete files written by cgx
 import platform # need this to check for win and do the below dpi fix
+import ctypes # needed to see if it's 32-bit and to get correct win version
+
+# http://stackoverflow.com/questions/19128219/detect-windows-8-1-in-python/22325767#22325767
+class OSVERSIONINFOEXW(ctypes.Structure):
+    _fields_ = [('dwOSVersionInfoSize', ctypes.c_ulong),
+                ('dwMajorVersion', ctypes.c_ulong),
+                ('dwMinorVersion', ctypes.c_ulong),
+                ('dwBuildNumber', ctypes.c_ulong),
+                ('dwPlatformId', ctypes.c_ulong),
+                ('szCSDVersion', ctypes.c_wchar*128),
+                ('wServicePackMajor', ctypes.c_ushort),
+                ('wServicePackMinor', ctypes.c_ushort),
+                ('wSuiteMask', ctypes.c_ushort),
+                ('wProductType', ctypes.c_byte),
+                ('wReserved', ctypes.c_byte)]
+
+def get_version():
+    """
+    Get's the OS major.minor version. Returns a float of OS_MAJOR.OS_MINOR
+    """
+    os_version = OSVERSIONINFOEXW()
+    os_version.dwOSVersionInfoSize = ctypes.sizeof(os_version)
+    retcode = ctypes.windll.Ntdll.RtlGetVersion(ctypes.byref(os_version))
+    if retcode != 0:
+        print("Failed to get OS version")
+        return -1.0
+
+    major = str(os_version.dwMajorVersion)
+    minor = str(os_version.dwMinorVersion)
+    return float(major+'.'+minor)
 
 def get_dpi():
     """Returns an int of current DPI for windows or None."""
     if platform.system() == 'Windows':
-        if float(platform.release()) >= 8:
+        version = get_version()
+        if version >= 6.2:
+            # Windows 8.0 is 6.2.9200
             # Only run on windows 8 or higher to check high dpi monitors
 
-            import ctypes
             user32 = ctypes.windll.user32
             w_curr = user32.GetSystemMetrics(0)
             user32.SetProcessDPIAware()
